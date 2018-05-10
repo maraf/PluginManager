6f33caea1fee9b99f9e57f69a7ca3041a4f5f5ee
Marek Fišera <mara@neptuo.com>
2018-05-10 19:37:12 +0200

    Add basic project structure.

---
+++ src/PackageManager.UI/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PackageManager.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PackageManager
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow wnd = new MainWindow();
+            wnd.Show();
+        }
+    }
+}
