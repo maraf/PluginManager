77912e0ce787a30260cedec7d0fb1a509e421bba
Marek Fišera <mara@neptuo.com>
2018-05-10 19:49:30 +0200

    #1 - Search header of Browser.

---
+++ src/PackageManager.UI/Views/DesignData/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using PackageManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager.Views.DesignData
+{
+    public static class ViewModelLocator
+    {
+        private static BrowserViewModel browser;
+
+        public static BrowserViewModel Browser
+        {
+            get
+            {
+                if (browser == null)
+                {
+                    browser = new BrowserViewModel()
+                    {
+                        SearchText = "GitExtensions",
+                        Source = "https://api.nuget.org/v3/index.json"
+                    };
+                }
+
+                return browser;
+            }
+        }
+    }
+}
---
+++ src/PackageManager/ViewModels/BrowserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Neptuo.Observables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager.ViewModels
+{
+    public class BrowserViewModel : ObservableObject
+    {
+        private string source;
+        public string Source
+        {
+            get { return source; }
+            set
+            {
+                if (source != value)
+                {
+                    source = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                if (searchText != value)
+                {
+                    searchText = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+    }
+}
