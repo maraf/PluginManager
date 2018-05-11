6e2489272c044bf4e3f88fa277808deb1ace0a77
Marek Fišera <mara@neptuo.com>
2018-05-12 00:00:34 +0200

    #1 - Fix package icons.

---
+++ src/PackageManager.UI/Views/Converters/NullValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PackageManager.Views.Converters
+{
+    public class NullValueConverter : IValueConverter
+    {
+        public object DefaultValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return DefaultValue;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
