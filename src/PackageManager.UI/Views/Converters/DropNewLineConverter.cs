edaa5ee431eea40c695518dd6c1841192eb81760
Marek Fišera <mara@neptuo.com>
2018-05-11 17:46:51 +0200

    #1 - Drop new lines in package list.

---
+++ src/PackageManager.UI/Views/Converters/DropNewLineConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PackageManager.Views.Converters
+{
+    public class DropNewLineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string target = value?.ToString();
+            if (String.IsNullOrEmpty(target))
+                return target;
+
+            target = target.Replace(Environment.NewLine, String.Empty);
+            target = target.Replace("\n", String.Empty);
+            return target;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
