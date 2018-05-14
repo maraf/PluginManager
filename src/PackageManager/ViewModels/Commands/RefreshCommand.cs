c601614a3ec8c40520318d3a24b1d18c3c52ceec
Marek Fišera <mara@neptuo.com>
2018-05-14 22:00:07 +0200

    #2 - View models for Installed packages.

---
+++ src/PackageManager/ViewModels/Commands/RefreshCommand.cs
@@ -0,0 +1,35 @@
+﻿using Neptuo;
+using Neptuo.Observables.Commands;
+using PackageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PackageManager.ViewModels.Commands
+{
+    public class RefreshCommand : Command
+    {
+        private readonly InstalledViewModel viewModel;
+        private readonly IInstallService service;
+
+        public RefreshCommand(InstalledViewModel viewModel, IInstallService service)
+        {
+            Ensure.NotNull(viewModel, "viewModel");
+            Ensure.NotNull(service, "service");
+            this.viewModel = viewModel;
+            this.service = service;
+        }
+
+        public override bool CanExecute()
+            => true;
+
+        public override void Execute()
+        {
+            viewModel.Items.Clear();
+            viewModel.Items.AddRange(service.GetInstalled());
+        }
+    }
+}
---
+++ src/PackageManager/ViewModels/InstalledViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Neptuo;
+using Neptuo.Observables;
+using Neptuo.Observables.Collections;
+using PackageManager.Models;
+using PackageManager.Services;
+using PackageManager.ViewModels.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PackageManager.ViewModels
+{
+    public class InstalledViewModel : ObservableObject
+    {
+        private readonly IInstallService service;
+
+        public ObservableCollection<IPackage> Items { get; }
+        public ICommand Refresh { get; }
+        public UninstallCommand Uninstall { get; }
+
+        public InstalledViewModel(IInstallService service)
+        {
+            Ensure.NotNull(service, "service");
+            this.service = service;
+
+            Items = new ObservableCollection<IPackage>();
+            Refresh = new 
+            Uninstall = new UninstallCommand(service);
+        }
+    }
+}
