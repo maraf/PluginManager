354e1cb7e1fe38da52e42f7c55d3715c6d0d98fc
Marek Fišera <mara@neptuo.com>
2018-05-16 19:21:24 +0200

    #11 - Enable whole UI only when valid package source URL is provided.

---
+++ src/PackageManager.UI/Views/Converters/ValidUrlToTrueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PackageManager.Views.Converters
+{
+    public class ValidUrlToTrueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string url = (string)value;
+            return Uri.TryCreate(url, UriKind.Absolute, out Uri uri);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
