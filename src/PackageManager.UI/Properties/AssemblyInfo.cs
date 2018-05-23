10330c6579c32208015a58272116497d0a6f0327
Marek Fišera <mara@neptuo.com>
2018-05-23 17:26:04 +0200

    Add version info.

--- src/PackageManager.UI/Properties/AssemblyInfo.cs
+++ src/PackageManager.UI/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using PackageManager;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -7,11 +8,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PackageManager.UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PackageManager")]
+[assembly: AssemblyDescription("NuGet based package manager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PackageManager.UI")]
+[assembly: AssemblyCompany("Neptuo")]
+[assembly: AssemblyProduct("PackageManager")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +52,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.Preview)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
---
+++ src/PackageManager.UI/Properties/VersionInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager
+{
+    public static class VersionInfo
+    {
+        internal const string Version = "0.1.0";
+        internal const string Preview = null; //-beta1
+
+        public static Version GetVersion()
+        {
+            return new Version(Version);
+        }
+    }
+}
