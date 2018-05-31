ef416b1941df62ffd429c482ac9469a0a6637c9c
Marek Fišera <mara@neptuo.com>
2018-05-31 16:19:28 +0200

    #19 - Refactor constants for running PluginManager from GE.

--- src/GitExtensions.PluginManager/Plugin.cs
+++ src/GitExtensions.PluginManager/Plugin.cs
@@ -16,6 +16,11 @@ namespace GitExtensions.PluginManager
     /// </summary>
     public class Plugin : GitPluginBase, IGitPluginForRepository
     {
+        public const string GitExtensionsRelativePath = @"GitExtensions.exe";
+        public const string PluginManagerRelativePath = @"PluginManager\PackageManager.UI.exe";
+        public const string PluginsPackageNameFormat = "--dependencies GitExtensions.Plugins-v{0}.{1}";
+        public static readonly List<string> FrameworkMonikers = new List<string>() { "net461", "net462", "any", "netstandard2.0" };
+
         internal PluginSettings Configuration { get; private set; }
 
         public Plugin()
@@ -24,35 +29,39 @@ namespace GitExtensions.PluginManager
             Description = "Plugin Manager";
         }
 
+        public override void Register(IGitUICommands commands)
+        {
+            base.Register(commands);
+            Configuration = new PluginSettings(Settings);
+        }
+
+        public override IEnumerable<ISetting> GetSettings()
+            => Configuration;
+
         public override bool Execute(GitUIBaseEventArgs gitUiCommands)
         {
-            string basePath = @"C:\Program Files (x86)\GitExtensions";
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
             string pluginsPath = Path.Combine(basePath, "Plugins");
 
-            FileVersionInfo version = FileVersionInfo.GetVersionInfo(Path.Combine(basePath, "GitExtensions.exe"));
+            FileVersionInfo version = FileVersionInfo.GetVersionInfo(Path.Combine(basePath, GitExtensionsRelativePath));
+
+            List<string> args = new List<string>();
+            args.Add($"--path \"{pluginsPath}\"");
+            args.Add(String.Format(PluginsPackageNameFormat, version.ProductMajorPart, version.ProductMinorPart));
+            args.Add($"--monikers {String.Join(",", FrameworkMonikers)}");
+
+            if (Uri.IsWellFormedUriString(Configuration.PackageSourceUrl, UriKind.Absolute))
+                args.Add($"--packagesource {Configuration.PackageSourceUrl}");
 
             ProcessStartInfo info = new ProcessStartInfo(
-                Path.Combine(pluginsPath, @"PluginManager\PackageManager.UI.exe"),
-                $"--path \"{pluginsPath}\" " +
-                $"--dependencies GitExtensions.Plugins-v{version.ProductMajorPart}.{version.ProductMinorPart} " +
-                $"--monikers net461,net462,any,netstandard2.0 " +
-                $"--packagesource {Configuration.PackageSourceUrl}"
+                Path.Combine(pluginsPath, PluginManagerRelativePath),
+                String.Join(" ", args)
             );
             info.UseShellExecute = false;
             info.Verb = "runas";
 
             Process.Start(info);
             return true;
-        }
-
-        public override IEnumerable<ISetting> GetSettings()
-            => Configuration;
-
-        public override void Register(IGitUICommands commands)
-        {
-            base.Register(commands);
-
-            Configuration = new PluginSettings(Settings);
         }
     }
 }
