62246b767f5e25e2f8c838e78b5e82abe69adeda
Marek Fišera <mara@neptuo.com>
2018-07-09 19:52:40 +0200

    #20 - Project and argument processing.

---
+++ src/PackageManager.Cli/Args.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager
+{
+    public partial class Args
+    {
+        public string Path { get; set; }
+        public string PackageSourceUrl { get; set; }
+
+        public bool IsUpdateCount { get; set; }
+    }
+}
---
+++ src/PackageManager.Cli/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace PackageManager.Cli
+{
+    class Program
+    {
+        public static Args Args { get; private set; }
+
+        static void Main(string[] args)
+        {
+            if (ParseParameters(args))
+            {
+
+            }
+        }
+
+        private static bool ParseParameters(string[] args)
+        {
+            Args = new Args();
+
+            int skipped = 0;
+            if (args[0] == "update")
+            {
+                skipped++;
+                if (args[1] == "--count")
+                {
+                    Args.IsUpdateCount = true;
+                    skipped++;
+                }
+            }
+
+            if ((args.Length - skipped) % 2 == 0)
+            {
+                for (int i = 0; i < args.Length; i += 2)
+                {
+                    string name = args[i];
+                    string value = args[i + 1];
+                    ParseParameter(name, value);
+                }
+            }
+
+            if (!Directory.Exists(Args.Path))
+            {
+                Console.WriteLine("Missing argument '--path' - a target path to install packages to.");
+                return false;
+            }
+
+            if (!Uri.IsWellFormedUriString(Args.PackageSourceUrl, UriKind.Absolute))
+            {
+                Console.WriteLine("Missing argument '--path' - a target path to install packages to.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool ParseParameter(string name, string value)
+        {
+            switch (name)
+            {
+                case "--path":
+                    Args.Path = value;
+                    return true;
+                case "--packagesource":
+                    Args.PackageSourceUrl = value;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
