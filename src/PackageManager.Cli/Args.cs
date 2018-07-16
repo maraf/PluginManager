cf9e9f82c33cbd2876909945cf05e9d5716a9a09
Marek Fišera <mara@neptuo.com>
2018-07-16 18:12:45 +0200

    #20 - Move parameter parsing to Args class

--- src/PackageManager.Cli/Args.cs
+++ src/PackageManager.Cli/Args.cs
@@ -15,5 +15,55 @@ namespace PackageManager
         public bool IsUpdateCount { get; set; }
 
         string IPackageSourceProvider.Url => PackageSourceUrl;
+
+        public Args(string[] args)
+        {
+            ParseParameters(args);
+        }
+
+        #region Parse
+
+        private bool ParseParameters(string[] args)
+        {
+            int skipped = 0;
+            if (args[0] == "update")
+            {
+                skipped++;
+                if (args[1] == "--count")
+                {
+                    IsUpdateCount = true;
+                    skipped++;
+                }
+            }
+
+            if ((args.Length - skipped) % 2 == 0)
+            {
+                for (int i = skipped; i < args.Length; i += 2)
+                {
+                    string name = args[i];
+                    string value = args[i + 1];
+                    ParseParameter(name, value);
+                }
+            }
+
+            return true;
+        }
+
+        private bool ParseParameter(string name, string value)
+        {
+            switch (name)
+            {
+                case "--path":
+                    Path = value;
+                    return true;
+                case "--packagesource":
+                    PackageSourceUrl = value;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        #endregion
     }
 }
