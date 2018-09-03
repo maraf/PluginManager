4489bc1ce9f064aefa8da4292f1410eabb260037
Marek Fišera <mara@neptuo.com>
2018-09-03 18:15:50 +0200

    #24 - Test cases for update.

---
+++ test/PackageManager.Tests/ViewModels/Commands/SelfUpdateService.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using PackageManager.Models;
+using PackageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager.ViewModels.Commands
+{
+    public class SelfUpdateService
+    {
+        public ISelfUpdateService Object { get; }
+        public bool IsIsSelfUpdateCalled { get; private set; }
+        public bool IsUpdateCalled { get; private set; }
+        public bool IsRunNewInstanceCalled { get; private set; }
+
+        public SelfUpdateService(bool isSelfUpdate, Package updatePackage)
+        {
+            Mock<ISelfUpdateService> mock = new Mock<ISelfUpdateService>();
+
+            mock
+                .SetupGet(s => s.IsSelfUpdate)
+                .Callback(() => IsIsSelfUpdateCalled = true)
+                .Returns(isSelfUpdate);
+
+            mock
+                .Setup(s => s.Update(It.Is<IPackage>(p => p == updatePackage.Object)))
+                .Callback(() => IsUpdateCalled = true);
+
+            mock
+                .Setup(s => s.RunNewInstance(It.Is<IPackage>(p => p == updatePackage.Object)))
+                .Callback(() => IsRunNewInstanceCalled = true);
+
+            Object = mock.Object;
+        }
+    }
+}
--- test/PackageManager.Tests/ViewModels/Commands/TestCommands.cs
+++ test/PackageManager.Tests/ViewModels/Commands/TestCommands.cs
@@ -14,13 +14,13 @@ namespace PackageManager.ViewModels.Commands
     [TestClass]
     public class TestCommands
     {
+        private const string ExtractPath = @"D:\";
+
         [TestMethod]
         public void Install()
         {
-            const string extractPath = @"D:\";
-
-            var package = new Package(extractPath, "Test");
-            var install = new InstallService(extractPath, package);
+            var package = new Package(ExtractPath, "Test");
+            var install = new InstallService(ExtractPath, package);
 
             var command = new InstallCommand(install.Object);
             command.ExecuteAsync(package.Object).Wait();
@@ -33,10 +33,8 @@ namespace PackageManager.ViewModels.Commands
         [TestMethod]
         public void Uninstall()
         {
-            const string extractPath = @"D:\";
-
-            var package = new Package(extractPath, "Test");
-            var install = new InstallService(extractPath, null, package, package);
+            var package = new Package(ExtractPath, "Test");
+            var install = new InstallService(ExtractPath, null, package, package);
 
             var command = new UninstallCommand(install.Object, new SelfPackageConfiguration(null));
             Assert.IsTrue(command.CanExecute(package.Object));
@@ -50,15 +48,75 @@ namespace PackageManager.ViewModels.Commands
         }
 
         [TestMethod]
-        public void UninstallSelf()
+        public void Uninstall_Self()
         {
-            const string extractPath = @"D:\";
-
-            var package = new Package(extractPath, "Test");
-            var install = new InstallService(extractPath, null, package, package);
+            var package = new Package(ExtractPath, "Test");
+            var install = new InstallService(ExtractPath, null, package, package);
 
             var command = new UninstallCommand(install.Object, new SelfPackageConfiguration("Test"));
             Assert.IsFalse(command.CanExecute(package.Object));
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            var package = new Package(ExtractPath, "Test");
+            var viewModel = new PackageUpdateViewModel(package.Object, package.Object, false);
+            var install = new InstallService(ExtractPath, package, package, package);
+            var selfUpdate = new SelfUpdateService(false, null);
+
+            var command = new UpdateCommand(install.Object, selfUpdate.Object);
+
+            Assert.IsTrue(command.CanExecute(viewModel));
+
+            command.ExecuteAsync(viewModel).Wait();
+
+            Assert.IsFalse(selfUpdate.IsIsSelfUpdateCalled);
+            Assert.IsFalse(selfUpdate.IsRunNewInstanceCalled);
+            Assert.IsTrue(install.IsUninstallCalled);
+            Assert.IsTrue(install.IsInstallCalled);
+        }
+
+        [TestMethod]
+        public void Update_Self()
+        {
+            var package = new Package(ExtractPath, "Test");
+            var viewModel = new PackageUpdateViewModel(package.Object, package.Object, true);
+            var install = new InstallService(ExtractPath, package, package, package);
+            var selfUpdate = new SelfUpdateService(false, package);
+
+            var command = new UpdateCommand(install.Object, selfUpdate.Object);
+
+            Assert.IsTrue(command.CanExecute(viewModel));
+
+            command.ExecuteAsync(viewModel).Wait();
+
+            Assert.IsTrue(selfUpdate.IsIsSelfUpdateCalled);
+            Assert.IsTrue(selfUpdate.IsUpdateCalled);
+            Assert.IsFalse(selfUpdate.IsRunNewInstanceCalled);
+            Assert.IsFalse(install.IsUninstallCalled);
+            Assert.IsFalse(install.IsInstallCalled);
+        }
+
+        [TestMethod]
+        public void Update_SelfComplete()
+        {
+            var package = new Package(ExtractPath, "Test");
+            var viewModel = new PackageUpdateViewModel(package.Object, package.Object, true);
+            var install = new InstallService(ExtractPath, package, package, package);
+            var selfUpdate = new SelfUpdateService(true, package);
+
+            var command = new UpdateCommand(install.Object, selfUpdate.Object);
+
+            Assert.IsTrue(command.CanExecute(viewModel));
+
+            command.ExecuteAsync(viewModel).Wait();
+
+            Assert.IsTrue(selfUpdate.IsIsSelfUpdateCalled);
+            Assert.IsFalse(selfUpdate.IsUpdateCalled);
+            Assert.IsTrue(selfUpdate.IsRunNewInstanceCalled);
+            Assert.IsTrue(install.IsUninstallCalled);
+            Assert.IsTrue(install.IsInstallCalled);
         }
     }
 }
