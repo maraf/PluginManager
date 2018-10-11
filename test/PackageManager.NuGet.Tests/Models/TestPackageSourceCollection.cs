23c63478fc5e3343407b1cc66fcbc2e08ad34756
Marek Fišera <mara@neptuo.com>
2018-10-11 20:26:49 +0200

    #39 - Infrastructure for testing NuGet implementation.

---
+++ test/PackageManager.NuGet.Tests/Models/TestPackageSourceCollection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageManager.Models
+{
+    [TestClass]
+    public class TestPackageSourceCollection
+    {
+        public const string ConfigFilePath = "PackageSource.config";
+
+        [TestMethod]
+        public void Crud()
+        {
+            EnsureConfigDeleted();
+
+            var sources = new NuGetPackageSourceCollection(new PackageSourceProvider(new Settings(Environment.CurrentDirectory, ConfigFilePath)));
+        }
+
+        private static void EnsureConfigDeleted()
+        {
+            if (File.Exists(ConfigFilePath))
+                File.Delete(ConfigFilePath);
+        }
+    }
+}
