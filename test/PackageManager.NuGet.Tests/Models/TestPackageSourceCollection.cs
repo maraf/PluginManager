4f0ea8bc7ae5ec5c690a6ab038eec04f3e128993
Marek Fišera <mara@neptuo.com>
2018-10-12 13:43:28 +0200

    #39 - Separate test for config file creation.

--- test/PackageManager.NuGet.Tests/Models/TestPackageSourceCollection.cs
+++ test/PackageManager.NuGet.Tests/Models/TestPackageSourceCollection.cs
@@ -26,6 +26,16 @@ namespace PackageManager.Models
         }
 
         [TestMethod]
+        public void CreateNewConfig()
+        {
+            EnsureConfigDeleted();
+
+            var sources = CreateSourceCollection();
+            Assert.IsNull(sources.Primary);
+            Assert.AreEqual(1, sources.All.Count);
+        }
+
+        [TestMethod]
         public void Crud()
         {
             EnsureConfigDeleted();
