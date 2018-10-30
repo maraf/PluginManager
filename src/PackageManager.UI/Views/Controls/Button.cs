c84705764880b0ef8d96be94e8f3f38d65060e9a
Marek Fišera <mara@neptuo.com>
2018-10-30 16:34:47 +0100

    #48 - Icons for Package Source window.

---
+++ src/PackageManager.UI/Views/Controls/Button.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PackageManager.Views.Controls
+{
+    public class Button
+    {
+        public static ImageSource GetImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(ImageProperty);
+        }
+
+        public static void SetImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(ImageProperty, value);
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.RegisterAttached(
+            "Image",
+            typeof(ImageSource),
+            typeof(Button),
+            new PropertyMetadata(null, OnImageChanged)
+        );
+
+        private static void OnImageChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContentControl view = (ContentControl)d;
+            UpdateContent(view, null, view.Content as TextBlock);
+        }
+
+
+        public static string GetText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(TextProperty);
+        }
+
+        public static void SetText(DependencyObject obj, string value)
+        {
+            obj.SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(
+            "Text",
+            typeof(string),
+            typeof(Button),
+            new PropertyMetadata(null, OnTextChanged)
+        );
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContentControl view = (ContentControl)d;
+            UpdateContent(view, view.Content as Image, null);
+        }
+
+
+        private static void UpdateContent(ContentControl view, Image image, TextBlock text)
+        {
+            if (image == null)
+            {
+                ImageSource imageSource = GetImage(view);
+                if (imageSource != null)
+                {
+                    image = new Image()
+                    {
+                        SnapsToDevicePixels = true,
+                        Source = GetImage(view)
+                    };
+                }
+            }
+
+            if (text == null)
+            {
+                string textSource = GetText(view);
+                if (textSource != null)
+                {
+                    text = new TextBlock()
+                    {
+                        Text = GetText(view)
+                    };
+                }
+            }
+
+            if (image != null && text != null)
+            {
+                var panel = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal,
+                };
+                view.Content = panel;
+
+                image.Height = 16;
+                panel.Children.Add(image);
+
+                text.Margin = new Thickness(4, 0, 0, 0);
+                panel.Children.Add(text);
+            }
+            else if (image != null)
+            {
+                if (view.Style == null)
+                    view.Style = (Style)Application.Current.Resources["ImageButton"];
+
+                image.Height = 22;
+                view.Content = image;
+            }
+            else if (text != null)
+            {
+                view.Content = text;
+            }
+        }
+    }
+}
