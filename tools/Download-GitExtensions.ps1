525ebd49d534722e029da62aad02567039181e1d
Marek Fišera <mara@neptuo.com>
2018-12-05 19:40:35 +0100

    #53 - Integrate GE reference download from BundleBackuper.

---
+++ tools/Download-GitExtensions.ps1
@@ -0,0 +1,37 @@
+Set-Location $PSScriptRoot
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ExtractRootPath = '../references'
+$AssetToDownloadName = 'GitExtensions-Portable-3.00.00.02-rc1.zip';
+$AssetToDownloadUrl = 'https://github.com/gitextensions/gitextensions/releases/download/v3.00.00-rc1/GitExtensions-Portable-3.00.00.02-rc1.zip';
+
+if (!($null -eq $AssetToDownloadUrl))
+{
+    $DownloadName = [System.IO.Path]::GetFileName($AssetToDownloadName);
+    $DownloadFilePath = [System.IO.Path]::Combine($ExtractRootPath, $DownloadName);
+    $ExtractPath = $ExtractRootPath;
+
+    if (!(Test-Path $DownloadFilePath))
+    {
+        if (!(Test-Path $ExtractRootPath))
+        {
+            New-Item -ItemType directory -Path $ExtractRootPath | Out-Null;
+        }
+
+        if (!(Test-Path $ExtractPath))
+        {
+            New-Item -ItemType directory -Path $ExtractPath | Out-Null;
+        }
+
+        Write-Host ('Downloading "' + $DownloadName + '".');
+
+        Invoke-WebRequest -Uri $AssetToDownloadUrl -OutFile $DownloadFilePath;
+        Expand-Archive $DownloadFilePath -DestinationPath $ExtractPath -Force
+    }
+    else 
+    {
+        Write-Host ('Download "' + $DownloadName + '" already exists.');
+    }
+}
+
+Pop-Location
